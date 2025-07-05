0cd70e558294298a39130252c2d7c985c58c05ac
ShashiSal98 <shashi.salwathura78@gmail.com>
2025-07-05 15:18:27 +0530

    Initial commit of homyz frontend

---
+++ src/utils/accordion.jsx
@@ -0,0 +1,23 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck/>,
+    heading: "Best interest rates on the market",
+    detail:
+      "Discover the most competitive interest rates available today—carefully selected to help you maximize your savings or minimize your borrowing costs. Whether you're looking for a high-yield savings account, a fixed deposit, or a low-interest loan, our up-to-date comparisons ensure you get the best value for your money.",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Prevent unstable prices",
+    detail:
+      "Implement strategies to maintain price stability and protect against sudden market fluctuations. Through data-driven insights, risk management, and consistent pricing policies, ensure predictable costs for both your business and customers—building trust and long-term value.",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Best price on the market",
+    detail:
+      "Get unmatched value with the most competitive pricing available. Our offerings are carefully benchmarked against industry standards to ensure you receive the best deal—without compromising on quality or performance.",
+  },
+];
+export default data;
